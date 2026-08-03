bd702840d08fa97cf99a06082ebfe9a9cd813504
Luis200410 <162214899+Luis200410@users.noreply.github.com>
2026-08-02 22:18:13 -0400

    separate folders so we only stay here with the landing and work towards the landing page of our apps

--- app/(app)/(billing)/layout.tsx
+++
@@ -1,5 +0,0 @@
-'use client'
-
-export default function BillingLayout({ children }: { children: React.ReactNode }) {
-    return children
-}
--- app/(app)/systems/money/page.tsx
+++
@@ -1,9 +0,0 @@
-import { MoneyOverview } from "@/components/money/overview";
-
-export default function MoneySystemRoot() {
-  return (
-    <main className="min-h-screen bg-black pt-12">
-      <MoneyOverview />
-    </main>
-  );
-}
