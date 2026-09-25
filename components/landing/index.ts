61f6de8a99cf696b73754240991e997d751bcfab
Luis200410 <162214899+Luis200410@users.noreply.github.com>
2026-09-25 15:51:34 -0400

    clear old components not being used

--- components/landing/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as CircularSplitRoll } from './circular-split-roll';
-export { default as ImmersiveFullscreenNav } from './immersive-fullscreen-nav';
-export { ImproveLogo, AnimatedLogoSvg } from './improve-logo';
-export { TheSystemSection } from './the-system-section';
